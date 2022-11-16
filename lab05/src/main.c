bd59affac252a50e310e248640c103701925ba6f
Slava522 <vogymggvpr@gmail.com>
2022-11-16 22:32:02 +0100

    ""

--- lab05/src/main.c
+++ lab05/src/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-int main() { 
-	int first_number;
-	int second_number;
-	printf("введіть першу цифру");
-	scanf("%d", first_number);
-	printf("введіть другу цифру");
-	scanf("%d", second_number);
-	
-	
+int main()
+{
+    int number1;
+    int number2;
+    int output = 0;
+    
+    printf("введіть перше число: ");
+    scanf("%u", &number1);
+    printf("введіть друге число: ");
+    scanf("%u", &number2);
 
-	return 0;}
+    for (int i = number1; i <= number2; i++) {
+        output = output + i;
+    }
+    printf("%u", output);
+    return 0;
+}
